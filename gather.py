0668dafbbe848d54014cbfbf355bee58e5649f3a
Ethan Fry <ethanffry@gmail.com>
2022-06-17 07:24:36 -0700

    absoluting paths

--- gather.py
+++ gather.py
@@ -1,3 +1,5 @@
+#! /home/pi/work/fast_enough/venv_fast_enough/bin/python3
+
 import json
 import subprocess
 
@@ -6,10 +8,10 @@ import subprocess
 process = subprocess.run(['/usr/local/bin/speedtest','-f', 'json'], check=True, stdout=subprocess.PIPE, universal_newlines=True)
 output = process.stdout
 
-fp = open('data.json', 'r')
+fp = open('/home/pi/work/fast_enough/data.json', 'r')
 data = json.load(fp)
 data.append(json.loads(output))
 fp.close()
-fp = open('data.json', 'w')
+fp = open('/home/pi/work/fast_enough/data.json', 'w')
 json.dump(data, fp)
 print(output)
