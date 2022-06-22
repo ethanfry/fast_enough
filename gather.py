c4cd032df77e76857bca51cd9ab8fed29f937f18
Ethan Fry <ethanffry@gmail.com>
2022-06-21 17:38:27 -0700

    moving config into separate file

--- gather.py
+++ gather.py
@@ -2,11 +2,10 @@
 
 import boto3
 import json
+import os
 import subprocess
-from dynamodb_json import json_util
 
-AWS_ACCESS_KEY_ID = 'REDACTED'
-AWS_SECRET_ACCESS_KEY = 'REDACTED'
+from dynamodb_json import json_util
 
 
 # run() returns a CompletedProcess object if it was successful
@@ -19,10 +18,14 @@ with open('/home/pi/work/fast_enough/data.json', 'r') as fp:
     data = json.load(fp)
 data.append(json.loads(output))
 if len(data) > 50:
-    client = boto3.client('dynamodb', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    config = None
+    with open(os.path.join(dir_path, 'config.json'), 'r') as conf_fp:
+        config = json.load(conf_fp)
+    client = boto3.client('dynamodb', region_name='us-west-2', aws_access_key_id=config['AWS_ACCESS_KEY_ID'], aws_secret_access_key=config['AWS_SECRET_ACCESS_KEY'])
     for row in data:
         client.put_item(TableName='fast_enough', Item=json_util.json.loads(json_util.dumps(row)))
-    data = {}
+    data = []
 
 with open('/home/pi/work/fast_enough/data.json', 'w') as fp:
     json.dump(data, fp)
