b488338a363e122f675a75d90f7bbdbc2baeea60
Ethan Fry <ethanffry@gmail.com>
2022-06-21 18:02:09 -0700

    updates

--- gather.py
+++ gather.py
@@ -4,29 +4,37 @@ import boto3
 import json
 import os
 import subprocess
+import time
 
 from dynamodb_json import json_util
 
+# The directory we're located in
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
-# run() returns a CompletedProcess object if it was successful
-# errors in the created process are raised here too
-process = subprocess.run(['/usr/local/bin/speedtest','-f', 'json'], check=True, stdout=subprocess.PIPE, universal_newlines=True)
-output = process.stdout
+# Get our configuration
+config = None
+with open(os.path.join(dir_path, 'config.json'), 'r') as conf_fp:
+    config = json.load(conf_fp)
 
-data = None
-with open('/home/pi/work/fast_enough/data.json', 'r') as fp:
-    data = json.load(fp)
-data.append(json.loads(output))
-if len(data) > 50:
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    config = None
-    with open(os.path.join(dir_path, 'config.json'), 'r') as conf_fp:
-        config = json.load(conf_fp)
-    client = boto3.client('dynamodb', region_name='us-west-2', aws_access_key_id=config['AWS_ACCESS_KEY_ID'], aws_secret_access_key=config['AWS_SECRET_ACCESS_KEY'])
-    for row in data:
-        client.put_item(TableName='fast_enough', Item=json_util.json.loads(json_util.dumps(row)))
-    data = []
+while True:
+    # run() returns a CompletedProcess object if it was successful
+    # errors in the created process are raised here too
+    process = subprocess.run(['/usr/local/bin/speedtest','-f', 'json'], check=True, stdout=subprocess.PIPE, universal_newlines=True)
+    output = process.stdout
 
-with open('/home/pi/work/fast_enough/data.json', 'w') as fp:
-    json.dump(data, fp)
-print(output)
+
+    # Check our local data cache. If there are too many entries, send them up to AWS
+    data = None
+    with open(os.path.join(dir_path, 'data.json'), 'r') as fp:
+        data = json.load(fp)
+    data.append(json.loads(output))
+    if len(data) > config['LOCAL_CACHE_LIMIT']:  
+        client = boto3.client('dynamodb', region_name='us-west-2', aws_access_key_id=config['AWS_ACCESS_KEY_ID'], aws_secret_access_key=config['AWS_SECRET_ACCESS_KEY'])
+        for row in data:
+            client.put_item(TableName='fast_enough', Item=json_util.json.loads(json_util.dumps(row)))
+        data = []
+    
+    with open(os.path.join(dir_path, 'data.json'), 'w') as fp:
+        json.dump(data, fp)
+    print(output)
+    time.sleep(config['PERIOD_IN_SECONDS'])
